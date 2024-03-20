a4deaff1634441a6c598b847579fcdf7fb5b86e8
Moses Mutisya <mutisyamoses722@gmail.com>
2024-03-20 17:18:08 +0300

    writing functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  const hq = 42;
+  return Math.abs(location - hq);
+}
+
+function distanceFromHqInFeet(location) {
+  const blocks = distanceFromHqInBlocks(location);
+  return blocks * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(destination - start);
+  return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
